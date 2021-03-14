cdfeffb6fd52a824eeaf51b975ffa5eb0e54542f
Quang <quang.nd90@gmail.com>
2021-03-14 15:29:05 +0100

    update

--- project/Datastreamer.py
+++
@@ -1,8 +0,0 @@
-# streamer class to fill word2vec list by list
-class DataStreamer():
-    def __init__(self, data):
-        self.data = data
-
-    def __iter__(self):
-        for basket in self.data:
-            yield basket.tolist()
